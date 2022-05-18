b364898a79e08dda73b7217b1794affce08f0897
Alberttuvi1 <alberttubert@inspladelestany.cat>
2022-05-18 16:01:31 +0200

    commit albert

---
+++ src/Maquina.java
@@ -0,0 +1,36 @@
+public class Maquina {
+    // Atributs
+    // llista productes
+    // moneder
+
+    // Constructor
+
+    // Mètodes
+
+    // ferVenta(String codi){
+    // Existeix Codi?
+    // Donat un codi -> saber el producte.
+    // Hi ha stock del producte?
+    // Demanar monedes.
+    // Calcular canvi.
+    // ...
+    // }
+
+    private void calcularCanvi()
+
+    private void demanarMonedes(Producte p) {
+        // Mostra per pantalla el preu del producte.
+    }
+
+    private boolean existeixCodi() {
+        // mira a la llista de productes si hi ha el codi que s'ha entrat
+    }
+
+    private Producte saberProducte(String codi) {
+        // Donat un codi, retorna el producte.
+    }
+
+    private boolean stockProducteExistent() {
+        // mirar si a la llista de productes
+    }
+}
